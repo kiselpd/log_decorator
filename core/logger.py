2a23e19b5d9a20d49d9b3be50a2274786d9abdac
Pavel Kiselev <k03iselev@yandex.ru>
2023-04-20 14:13:59 +0300

    Initial commit

---
+++ core/logger.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+
+def logger(old_function):
+    log_path = "main.log"
+
+    def new_function(*args, **kwargs):
+        log_list = []
+
+        function_name = old_function.__name__
+        function_args = f"args: {args}"
+        function_kwargs = f"kwargs: {kwargs}"
+
+        value = old_function(*args, **kwargs)
+
+        function_time = str(datetime.now())[:-7]
+        function_value = f"return: {value}"
+
+        with open(file=log_path, mode="a", newline="\n") as f:
+            f.write(f"{function_name} | {function_time} | {function_args} | {function_kwargs} | {function_value}\n")
+
+        return value
+    
+    return new_function
+
+
+def smart_logger(file_path):
+
+    def logger_(old_function):
+        log_path = file_path
+
+        def new_function(*args, **kwargs):
+            log_list = []
+
+            function_name = old_function.__name__
+            function_args = f"args: {args}"
+            function_kwargs = f"kwargs: {kwargs}"
+
+            value = old_function(*args, **kwargs)
+
+            function_time = str(datetime.now())[:-7]
+            function_value = f"return: {value}"
+
+            with open(file=log_path, mode="a", newline="\n") as f:
+                f.write(f"{function_name} | {function_time} | {function_args} | {function_kwargs} | {function_value}\n")
+
+            return value
+    
+        return new_function
+    
+    return logger_
---
+++ main.py
@@ -0,0 +1,11 @@
+import core.test as tests
+import os
+
+def main():
+    tests.test_1()
+    tests.test_2()
+    tests.test_3()
+
+
+if __name__=="__main__":
+    main()
